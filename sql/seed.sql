d1ba9d3e98080589c3f7dddde3d2edce0bd20e3c
ArikMackenburg <Arikmackenburg@gmail.com>
2020-08-17 14:21:09 -0500

    altered some small stuff

---
+++ sql/seed.sql
@@ -0,0 +1,22 @@
+-- CREATE TABLE IF NOT EXISTS vehicles (
+--   id SERIAL PRIMARY KEY,
+--   Imag_Url VARCHAR(100),
+--   "name" VARCHAR (500) NOT NULL,
+--   model VARCHAR (100) NOT NULL,
+--   "length" VARCHAR(5000) NOT NULL,
+--   cargo_capacity VARCHAR (8000) NOT NULL
+-- );
+-- INSERT INTO books (title,authors,image_url,description,isbn)
+-- VALUES ('Dragons Love Tacos 2: The Sequel',
+--   'Adam Rubin',
+--   'http://books.google.com/books/content?id=hKHbDAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+--   'The hilarious sequel to the smokin’ hot New York Times best seller, perfect for story time News alert! It has just been discovered that there are NO MORE TACOS left anywhere in the world. This is a huge problem because, as you know, dragons love tacos. If only there was a way for the dragons to travel back in time, to before tacos went extinct. Then they could grab lots of tacos and bring them back! It’s the perfect plan, as long as there’s no spicy salsa. You remember what happened last time . . . The award-winning creators of Robo-Sauce and Secret Pizza Party return with a gut-bustingly hilarious companion to the bestselling phenomenon Dragons Love Tacos.',
+--   '9780735229655, 0735229651');
+--  Vehicles {
+--     name: 'Bantha-II cargo skiff',
+--     model: 'Bantha-II',
+--     length: '9.5',
+--     cargo_capacity: '135000',
+--     image_url: 'bantha-iicargoskiff.jpg'
+--   }
+INSERT INTO vehicles (image_url)
